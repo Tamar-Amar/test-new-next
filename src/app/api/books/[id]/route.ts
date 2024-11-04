aaaf10fe81b9f3f27656affcc5888297f296267f
David Marom <davidmarom@hotmail.com>
2024-11-04 10:58:21 +0200

    first commit

---
+++ src/app/api/books/[id]/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request, params: { params: { id: string } }) {
+    const id = params.params.id;
+
+    // if you dont receive params from the client:
+    // const url = new URL(request.url);
+    // const id = url.pathname.split('/').pop();
+    return NextResponse.json({
+        id: id
+    });
+}
