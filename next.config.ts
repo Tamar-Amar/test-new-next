c23b814c6c843e306bec370790778789db63a53a
Tamar-Amar <amtamar747@gmail.com>
2024-11-05 11:16:05 +0200

    mondo

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env:{
+    PUBLIC_DB_CONNECTION: "mongodb+srv://amtamar747:<PUAMuWtoF9ghjil7>@cluster0.dgdjt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  }
 };
 
 export default nextConfig;
